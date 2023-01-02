83b9e7fde6d2fe7bff883c8853afcf58108b6f84
Mohammad Mehrabani <mehrabani.developer@gmail.com>
2023-01-02 21:29:26 +0330

    send fix data and show data in websockets:serve log

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/playground', function () {
+   event(new \App\Events\PlaygroundEvent());
+
+   return null;
+});
